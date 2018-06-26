68ea1fa5a1c9c2ba71f6c784eb85b50e9e35d72a
Chris Wang <wangguanzzz@126.com>
2018-06-26 16:27:03 +0800

    update

--- ansible/template/summary.rb
+++ ansible/template/summary.rb
@@ -1,17 +1,19 @@
-file = File.new("result.csv","r")
-counter = 0
-sum = 0
-while( line = file.gets )
-  next if counter == 0
-  if not line.include? 'OK'
-    puts 'failed!'
-  end
-  elapse = (line.split(',')[1]).to_i
-  sum += elapse
-  counter += 1
+files = Dir.glob("/*KB.csv")
+files.each do | file|
+        result = 0
+        number = 0
+        File.open(file,"r") do |f|
+                f.each_line do | line |
+                  temp = line.split(',')[1].to_i
+                  if temp > 0
+                    result += temp
+                    number += 1
+                  end
+                end
+        end
+        if number > 0
+          average = result / number
+          file = file[1..-1]
+          `echo "#{file},#{average}" >> /summary.csv`
+        end
 end
-puts sum
-puts counter
-puts (sum.to_f/counter.to_f)
-
-file.close
